df80e77c210e03eb6d2a62d41b3daf281f277ab3
Jamie Dawson <jamierobertdawson@gmail.com>
2024-08-23 23:22:04 -0700

    updated header to have link to GitHub page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,13 @@ const Header: React.FC = () => {
       Spotify Battle ground is a MERN stack application where you can paste the
       URL of a Spotify album into any of the 4 inputs and save them into a
       MongoDB database. So anyone seeing this site will see your music
-      recommendation.
+      recommendation. You can view the code by going to my{" "}
+      <a
+        href="https://github.com/JamieDawson/spotify-app-frontend-code"
+        target="_blank"
+      >
+        Github account.
+      </a>
     </div>
   );
 };
